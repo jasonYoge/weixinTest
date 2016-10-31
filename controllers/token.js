8975b3142545f0dc9bf3f2ff390c05bbc1f59dad
ywj <ppaii123@outlook.com>
2016-10-31 14:48:25 +0800

    change to token.js

--- controllers/token.js
+++ controllers/token.js
@@ -6,28 +6,29 @@ const sha1 = crypto.createHash('sha1');
 
 //  get token from local .txt file
 const TOKEN = fs.readFileSync(path.resolve(__dirname, '../const/token.txt'))
-    .toString();
+    .toString().replace(/[\r\n]/g, '');
 //  ensure token is exist
 if (!TOKEN) {
     throw new Error('token is undefined!');
 }
 //  token router
 const fn_token = function *() {
-    let timestamp = this.params.timestamp,
-        nonce = this.params.nonce,
-        signature = this.params.signature,
-        echostr = this.params.echostr;
+    let timestamp = this.query.timestamp,
+        nonce = this.query.nonce,
+        signature = this.query.signature,
+        echostr = this.query.echostr;
 
     let tmpArr = [TOKEN, timestamp, nonce].sort();
     let tmpStr = tmpArr.join('');
     //  sha1 validate
     sha1.update(tmpStr);
-    tmpStr = sha1.digest('base64');
+    tmpStr = sha1.digest('hex');
 
     if (tmpStr === signature) {
         this.body = echostr;
     } else {
-        console.log('token validation failed.');
+        this.body = 'token is worng.';
+        console.log('token worng.');
     }
 }
 //  export
